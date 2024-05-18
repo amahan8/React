8c0441845965c423ae6d51dfaa63d10c54943a02
Amir Ansari <amir@gmail.com>
2024-05-18 14:00:42 -0500

    Second commit

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -2,8 +2,37 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+function MyApp() {
+  return (
+    <div>
+      <h1>Custom App</h1>
+    </div>
+  );
+}
+
+const reactElement = {
+  type: "a",
+  props: {
+    href: "https://google.com",
+    target: "_blank",
+  },
+  children: "Click me to visit google",
+};
+
+const anotherElement = (
+  <a href="https://google.com" target="_blank">
+    Visit Google
+  </a>
 );
+
+const username = "Chai";
+const reactElment = React.createElement(
+  "a",
+  {
+    href: "https://google.com",
+    target: "_blank",
+  },
+  "Click me to visit  an google",
+  username
+);
+ReactDOM.createRoot(document.getElementById("root")).render(reactElment);
